0f073a81a1d2858446307b8dc7aa3c2177610eca
bhallashivam1997 <72436501+bhallashivam1997@users.noreply.github.com>
2021-07-22 16:11:45 +0530

    Create ex-2.go

---
+++ day-2/ex-2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var (
+	mutex sync.Mutex
+)
+
+func getRating(wg *sync.WaitGroup, totalRating *int){
+	defer wg.Done()
+	rand.Seed(time.Now().UnixNano())
+	sleepRand := rand.Intn(10)
+	time.Sleep(time.Duration(sleepRand)*time.Millisecond)
+	mutex.Lock()
+	score := rand.Intn(10)
+	*totalRating+=score
+	mutex.Unlock()
+}
+
+
+func main(){
+	totalRating := 0
+	var wg sync.WaitGroup
+	for i:=0;i<200;i++{
+		wg.Add(1)
+		getRating(&wg,&totalRating)
+	}
+	wg.Wait()
+	ans := float64(totalRating)/200
+	fmt.Println(ans)
+}
