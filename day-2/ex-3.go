a19172ea4d5bb36584934677e083e74e7f4e45cc
bhallashivam1997 <72436501+bhallashivam1997@users.noreply.github.com>
2021-07-22 16:12:04 +0530

    Create ex-3.go

---
+++ day-2/ex-3.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	mutex sync.Mutex
+)
+
+func deposit(balance *float64, amount float64){
+	mutex.Lock()
+	*balance+=amount
+	fmt.Println("Deposit Complete")
+	mutex.Unlock()
+}
+
+func withdraw(balance *float64, amount float64){
+	mutex.Lock()
+	if *balance<amount{
+		fmt.Println("Insufficient Balance !!")
+	}else{
+		*balance-=amount
+		fmt.Println("Withdrawal Complete")
+	}
+	mutex.Unlock()
+}
+
+func main(){
+	balance := 500.0
+	withdraw(&balance,1000)
+	time.Sleep(5*time.Microsecond)
+	deposit(&balance,500)
+	time.Sleep(5*time.Microsecond)
+	withdraw(&balance,1000)
+}
