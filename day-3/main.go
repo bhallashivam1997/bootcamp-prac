4721cbbaace106bfbba964762309fc73da50a701
shivam bhalla <bhalla.shivam1997@gmail.com>
2021-07-25 13:22:55 +0530

    merged first-api

---
+++ day-3/main.go
@@ -0,0 +1,21 @@
+//main.go
+package main
+import (
+	"bootcamp-prac/day-3/Config"
+	"bootcamp-prac/day-3/Models"
+	"bootcamp-prac/day-3/Routes"
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+var err error
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
+}
